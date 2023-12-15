a4a95054b100caa9e0f7e38b036fa2c6da36ccec
kristiafiliposki <filiposki.kristian1112@gmail.com>
2023-12-15 16:39:28 +0100

    first commit

---
+++ del.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>php</title>
+    <?php $nome="ciao" ?>
+    <?php $php="php"?>
+</head>
+<body>
+    <div><?php echo $nome . " $php"?></div>
+    <div></div>
+</body>
+</html>
